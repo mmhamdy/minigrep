0f6c57303ca3cd94d29e689c70a497633e551a6e
mmhamdy <mohamdy2772@gmail.com>
2023-07-30 15:28:22 +0300

    add a failing test for search

--- src/lib.rs
+++ src/lib.rs
@@ -19,10 +19,30 @@ impl Config {
     }
 }
 
+pub fn search<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.file_path)?;
 
     println!("With text: \n{contents}"); 
 
     Ok(()) 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive
+        pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
