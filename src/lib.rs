08e5fb343de3867a56e230efa5a526400c2dfb52
mmhamdy <mohamdy2772@gmail.com>
2023-07-31 08:44:06 +0300

    test a working version of case insensitive search

--- src/lib.rs
+++ src/lib.rs
@@ -31,8 +31,17 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
-pub fn search_case_insensitive<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
-    vec![]
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
