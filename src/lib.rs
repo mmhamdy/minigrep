4194f0b115ccd5aed7ead9065fbf1dffbba270d9
mmhamdy <mohamdy2772@gmail.com>
2023-07-31 09:37:00 +0300

    control case insensitivity using env variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use std::env;
 use std::fs;
 use std::error::Error;
 
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
@@ -14,8 +16,14 @@ impl Config {
 
         let query = args[1].clone();
         let file_path = args[2].clone();
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
     
-        Ok(Config {query, file_path})
+        Ok(Config {
+            query, 
+            file_path,
+            ignore_case,
+        })
     }
 }
 
@@ -47,7 +55,13 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
